8c4f260d5c877207afd7183d3ec44cd2c57ea4fd
rfor10 <rforeman@ucsd.edu>
2018-03-08 18:59:17 -0800

    Update alarm_check_m80_tall2.py
    added comments

--- alarm_check_m80_tall2.py
+++ alarm_check_m80_tall2.py
@@ -1,8 +1,10 @@
 from pi_remote_alarm import checkDSBtemperature, sendMail, checkRemoteAlarm, checkIP
 
+# Pin configuration for specific device
 no_pin = 10
 nc_pin = 8
 
+# DSB Temperature Sensor creates a folder with random characters to save temp
 minus20_tall_2 = '28-00042b357fff'
 
 #Check IP Address
@@ -14,6 +16,7 @@ try:
 	sendMail('minus80', minus80_alarm+'\n'+ip)
 except:
 	minus80_alarm = 'Failed to acquire minus80'
+
 # Check minus 20 tall 2
 minus20tall2_temp = checkDSBtemperature(minus20_tall_2)
 sendMail('minus_20_tall_2', str(minus20tall2_temp)+'\n'+ip)
